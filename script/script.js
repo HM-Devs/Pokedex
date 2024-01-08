ab1e6e466b375b9aef6e643d8f9a1c287d2437ed
Humster <hamzahmalik1@hotmail.com>
2024-01-08 17:58:03 +0000

    validation handling and HTTP response handling

--- script/script.js
+++ script/script.js
@@ -6,16 +6,23 @@ const  fetchApi = async (pkmnName) => {
     //Joining pokemon that have more than one word in their name (nidoran-f)
     pkmnApiName = pkmnName.split(' ').join('-').toLowerCase();
 
+
     const response = await fetch
         ('https://pokeapi.co/api/v2/pokemon/' + pkmnApiName);
-    
-    const pkmnData = await response.json();
-    return pkmnData;
+
+    if (response.status === 200) {
+        const pkmnData = await response.json();
+        return pkmnData;
+    }
+    return false;
 }
 
 
 search.addEventListener('change', async (event) => {
     const pkmnData = await fetchApi(event.target.value);
+
+    //Validation handling for pokemon that do not exist
+    if(!pkmnData) alert('Pokémon does not exist, please enter a valid Pokémon.');
 
     console.log(pkmnData);
 
